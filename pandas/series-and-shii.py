7b4056c94710ff282c3363a7468490c4b2097071
hoodielive <hoodielive@gmail.com>
2019-06-16 17:37:05 -0400

    create a series of predefined indexes and make one out of a dictionary

--- pandas/series-and-shii.py
+++ pandas/series-and-shii.py
@@ -1,4 +1,23 @@
 from pandas import Series
 
+# create a series
 a = Series([10,20,30,40,50])
 print(a)
+
+# just values
+print(a.values)
+
+# just index
+print(a.index)
+
+# create series with predefined index
+b = Series([100,200,300], index=['b', 'c', 'a'])
+print(b)
+
+# get values from predefined index
+print(b[['b', 'a']])
+
+# create series from python-dictionary 
+d = {'a': 1000, 'b': 2000, 'c': 3000} 
+s = Series(d)
+print(s)
