4292e74b36d0bfc0c11121cc235af07eafadb9ca
hoodielive <hoodielive@gmail.com>
2019-06-17 15:55:10 -0400

    sorting and ranking

--- functions-on-data/duplicate_index_values.py
+++ functions-on-data/duplicate_index_values.py
@@ -1,4 +1,44 @@
 from pandas import Series, DataFrame 
-from numpy as np
+import numpy as np
 
-s 
+s = Series([1,2,3,4,5], index=['a', 'b', 'a', 'c', 'd']) 
+
+print(s)
+
+print(s['a'])
+
+print(s.index.is_unique)
+print()
+# create dataFrame 
+d = DataFrame(np.arange(6).reshape((2,3)), columns=[1,2,3], index=['a','a'])
+print(d)
+print() 
+
+# check for unqiueness
+print(d.index.is_unique)
+print()
+d = DataFrame(np.arange(16).reshape((4,4)))
+
+print(d) 
+print()
+
+# calculate the sums
+print(d.sum())
+
+# the sum of each row 
+print(d.sum(axis=1))
+
+# the sum of the columns / #rows
+print(d.mean())
+
+# the rows only
+print(d.mean(axis=1))
+
+# how many elements are present in a column
+print(d.count())
+
+# max and min
+print(d.min())
+print(d.max())
+
+
