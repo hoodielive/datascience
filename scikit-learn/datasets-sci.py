972085071390703ddae9c3f50e8d73a8b66b5635
hoodielive <hoodielive@gmail.com>
2019-06-19 21:49:15 -0400

    Features and Labels # comments added

--- scikit-learn/datasets-sci.py
+++ scikit-learn/datasets-sci.py
@@ -2,8 +2,12 @@ from sklearn import datasets as ds
 
 iris = ds.load_iris()
 
+# sklearn.datasets.base.Bunch
 print(type(iris))
 
+# feature names for variables and target_names for target/label
 print(iris.keys())
 
+# display actual dataset
 print(iris.data)
+
