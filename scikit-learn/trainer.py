769aef0c3357aea4935e3628a871c7abbf3dc808
hoodielive <hoodielive@gmail.com>
2019-06-21 23:35:44 -0400

    whattha

--- scikit-learn/trainer.py
+++ scikit-learn/trainer.py
@@ -1,8 +1,8 @@
 from sklearn.model_selection import train_test_split 
 
-x = ""
-y = ""
-test_size = ""
+x = 4
+y = 2
+test_size = 10
 
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.3)
 
