84ae0e892a69caf13a4293258247e7b4ba0d0f48
hoodielive <hoodielive@gmail.com>
2019-06-27 20:18:03 -0400

    you can skips rows as well as load by specifying delimiter

--- loading_data/read-content-from-file.py
+++ loading_data/read-content-from-file.py
@@ -2,6 +2,8 @@ import pandas as pd
 
 #df1 = pd.read_table("./sample.csv", sep=',')
 df1 = pd.read_csv("./sample.csv", names=['a','b','c','d'])
-
+df2 = pd.read_csv("./colon.sep", sep=":")
+df2 = pd.read_csv("./colon.sep", sep=":", skiprows=[2])
 print(df1)
+print(df2)
 
