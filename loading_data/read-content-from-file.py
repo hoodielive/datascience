006db051c8c831578fa6eeee99a31d468b16e0ab
hoodielive <hoodielive@gmail.com>
2019-06-27 20:14:03 -0400

    names=['a','b','c','d']

--- loading_data/read-content-from-file.py
+++ loading_data/read-content-from-file.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-df1 = pd.read_csv("./sample.csv")
-df1 = pd.read_table("./sample.csv", sep=',')
+#df1 = pd.read_table("./sample.csv", sep=',')
+df1 = pd.read_csv("./sample.csv", names=['a','b','c','d'])
 
 print(df1)
 
